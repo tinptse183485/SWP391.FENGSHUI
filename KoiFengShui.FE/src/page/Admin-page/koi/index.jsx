dae272b79ed4479151b9f7e394feea01d67b1489
khoatdse183308 <khoatdse183308@fpt.edu.vn>
2024-10-07 20:09:16 +0700

    Small update with CRUD, image now can apply

--- KoiFengShui.FE/src/page/Admin-page/koi/index.jsx
+++ KoiFengShui.FE/src/page/Admin-page/koi/index.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react'; // Thêm useEffect và useState
-import Sidebar from '../../../components/sidebar/Sidebar';
-import './index.css';
-import api from '../../../config/axios';
-import { Table } from 'antd';
+import { useEffect, useState } from "react"; // Thêm useEffect và useState
+import "./index.css";
+import api from "../../../config/axios";
+import { Image, Table } from "antd";
 
 const Koi = () => {
   const [data, setData] = useState([]); // Khởi tạo state để lưu trữ dữ liệu
@@ -11,41 +10,42 @@ const Koi = () => {
     fetchData();
   }, []); // Chạy một lần khi component mount
 
-  
   const fetchData = async () => {
     try {
-      const response = await api.get('KoiVariety/GetAllKoi'); // Thay thế 'API_URL' bằng URL thực tế
-      console.log('Fetched data:', response.data); // Kiểm tra dữ liệu nhận được
+      const response = await api.get("KoiVariety/GetAllKoi"); // Thay thế 'API_URL' bằng URL thực tế
+      console.log("Fetched data:", response.data); // Kiểm tra dữ liệu nhận được
       setData(response.data); // Lưu trữ dữ liệu vào state
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
   const columns = [
     {
-      title: 'Koi Type',
-      dataIndex: 'koiType',
+      title: "Koi Type",
+      dataIndex: "koiType",
     },
     {
-      title: 'Image',
-      dataIndex: 'image',
+      title: "Image",
+      dataIndex: "image",
+      render: (image) => {
+        return <Image src={`/koi_image/${image}`} alt="" width={100}></Image>;
+      },
     },
     {
-      title: 'Element',
-      dataIndex: 'element',
+      title: "Element",
+      dataIndex: "element",
     },
     {
-      title: 'Description',
-      dataIndex: 'description',
+      title: "Description",
+      dataIndex: "description",
     },
   ];
   return (
     <div className="dashboard-container">
-     <Table dataSource={data} columns={columns} />
+      <Table dataSource={data} columns={columns} />
     </div>
   );
 };
 
 export default Koi;
-
